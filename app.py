fd9022769c71b4ccf5905a72719a4fd65f9f17b0
KestiNet <esakesti@gmail.com>
2024-09-02 17:33:28 +0100

    added app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')  # This will look for 'index.html' in the 'templates' folder
+
+@app.route('/professional_timeline')
+def professional_timeline():
+    return render_template('professional_timeline.html')  # This will look for 'professional_timeline.html' in the 'templates' folder
+
+if __name__ == "__main__":
+    app.run(debug=True)

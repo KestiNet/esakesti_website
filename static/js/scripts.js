73e5a83ba2e15c965ad421754f678f5b735e4257
KestiNet <esakesti@gmail.com>
2024-09-02 23:49:23 +0100

    picture etc

---
+++ static/js/scripts.js
@@ -0,0 +1,6 @@
+// Add this to your script.js file
+
+document.querySelector('.cta').addEventListener('click', function(e) {
+    e.preventDefault();
+    alert('More information coming soon!');
+});

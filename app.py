f6db47c9a23ef7f21446ec702f6268fae558d613
KestiNet <esakesti@gmail.com>
2024-09-08 17:23:23 +0100

    fixed some bugs

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('index.html')  # This will look for 'index.html' in the 'templates' folder
+    return render_template('index.html')
 
 @app.route('/professional_timeline')
 def professional_timeline():
-    return render_template('professional_timeline.html')  # This will look for 'professional_timeline.html' in the 'templates' folder
+    return render_template('professional_timeline.html')
 
 @app.route('/contact_me')
 def contact_me():
     return render_template('contact_me.html')
+
+@app.route('/tools')
+def tools():
+    return render_template('tools.html')
 
 if __name__ == "__main__":
     app.run(debug=True)

ea7ce160c0b445f4becd1506a01bddb1b8c61718
KestiNet <esakesti@gmail.com>
2024-09-13 15:08:25 +0100

    port number

--- app.py
+++ app.py
@@ -18,5 +18,7 @@ def contact_me():
 def tools():
     return render_template('tools.html')
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 8080))  # Use PORT environment variable or default to 8080
+    app.run(host='0.0.0.0', port=port)
